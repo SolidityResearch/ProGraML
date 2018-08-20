148e82a8e5d92bdb24f878eba12e573c538cdff0
Chris Cummins <chrisc.101@gmail.com>
2018-08-20 11:00:29 +0000

    Add a llvm_as module.
    [Exported from 743c40e8ea8ca828fd9270c6ac075e139a4d0fde]

--- compilers/llvm/llvm_as.py
+++ compilers/llvm/llvm_as.py
@@ -1,16 +1,3 @@
-# Copyright 2019 Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """A python wrapper around llvm-as, the LLVM assembler.
 
 llvm-as is part of the LLVM compiler infrastructure. See: http://llvm.org.
@@ -26,37 +13,35 @@ Usage:
 import subprocess
 import sys
 import typing
+from absl import app
+from absl import flags
+from absl import logging
+from phd.lib.labm8 import bazelutil
+from phd.lib.labm8 import system
 
 from compilers.llvm import llvm
-from labm8.py import app
-from labm8.py import bazelutil
-from labm8.py import system
 
-FLAGS = app.FLAGS
 
-app.DEFINE_integer(
-  "llvm_as_timeout_seconds",
-  60,
-  "The maximum number of seconds to allow process to run.",
-)
+FLAGS = flags.FLAGS
 
-_LLVM_REPO = "llvm_linux" if system.is_linux() else "llvm_mac"
+flags.DEFINE_integer(
+    'llvm_as_timeout_seconds', 60,
+    'The maximum number of seconds to allow process to run.')
+
+_LLVM_REPO = 'llvm_linux' if system.is_linux() else 'llvm_mac'
 
 # Path to llvm-as binary.
-LLVM_AS = bazelutil.DataPath(f"{_LLVM_REPO}/bin/llvm-as")
+LLVM_AS = bazelutil.DataPath(f'{_LLVM_REPO}/bin/llvm-as')
 
 
 class LlvmAsError(llvm.LlvmError):
   """An error from llvm-as."""
-
   pass
 
 
-def Exec(
-  args: typing.List[str],
-  stdin: typing.Optional[str] = None,
-  timeout_seconds: int = 60,
-) -> subprocess.Popen:
+def Exec(args: typing.List[str],
+         stdin: typing.Optional[str] = None,
+         timeout_seconds: int = 60) -> subprocess.Popen:
   """Run llvm-as.
 
   Args:
@@ -70,21 +55,17 @@ def Exec(
   Raises:
     LlvmTimeout: If llvm-as does not complete before timeout_seconds.
   """
-  cmd = ["timeout", "-s9", str(timeout_seconds), str(LLVM_AS)] + args
-  app.Log(3, "$ %s", " ".join(cmd))
+  cmd = ['timeout', '-s9', str(timeout_seconds), str(LLVM_AS)] + args
+  logging.debug('$ %s', ' '.join(cmd))
   process = subprocess.Popen(
-    cmd,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    stdin=subprocess.PIPE if stdin else None,
-    universal_newlines=True,
-  )
+      cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+      stdin=subprocess.PIPE if stdin else None, universal_newlines=True)
   if stdin:
     stdout, stderr = process.communicate(stdin)
   else:
     stdout, stderr = process.communicate()
   if process.returncode == 9:
-    raise llvm.LlvmTimeout(f"llvm-as timed out after {timeout_seconds}s")
+    raise llvm.LlvmTimeout(f'llvm-as timed out after {timeout_seconds}s')
   process.stdout = stdout
   process.stderr = stderr
   return process
@@ -104,5 +85,5 @@ def main(argv: typing.List[str]):
     sys.exit(1)
 
 
-if __name__ == "__main__":
-  app.RunWithArgs(main)
+if __name__ == '__main__':
+  app.run(main)
