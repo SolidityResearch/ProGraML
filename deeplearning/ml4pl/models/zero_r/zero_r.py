b7fa603d563f1d5c37c9a8bc0a29286b39befc7d
Chris Cummins <chrisc.101@gmail.com>
2019-10-26 18:02:02 +0000

    Add a Zero-R model.
    [Exported from 9d048c22278dc4604ae6a847ab96129b7f3cad49]

--- deeplearning/ml4pl/models/zero_r/zero_r.py
+++ deeplearning/ml4pl/models/zero_r/zero_r.py
@@ -1,129 +1,64 @@
 """A Zero-R baseline classifier."""
-import time
-from typing import Any
-from typing import Iterable
 
 import numpy as np
+import scipy.stats
+import typing
 
-from deeplearning.ml4pl.graphs.labelled import graph_tuple_database
-from deeplearning.ml4pl.models import batch as batchs
 from deeplearning.ml4pl.models import classifier_base
-from deeplearning.ml4pl.models import epoch
-from deeplearning.ml4pl.models import run
-from labm8.py import app
-from labm8.py import progress
+from deeplearning.ml4pl.models import log_database
+from labm8 import app
 
 FLAGS = app.FLAGS
 
-app.DEFINE_integer(
-  "zero_r_batch_size",
-  1000000,
-  "The number of graphs to process in a single batch.",
-)
 
-
-class ZeroR(classifier_base.ClassifierBase):
+class ZeroRNodeClassifier(classifier_base.ClassifierBase):
   """A Zero-R classifier that supports node-level or graph-level labels.
 
-  Zero-R classifiers predict the mode value from the training set. It is used
-  as a baseline for comparing the performance of other classifiers.
+  A Zero-R classifier always predicts the mode value from the training set. It
+  is used as a baseline for comparing the performance of other classifiers.
   """
 
-  def __init__(self, *args, **kwargs):
-    super(ZeroR, self).__init__(*args, **kwargs)
-    # The table used to count training labels.
-    self.class_counts = np.zeros(self.y_dimensionality, dtype=np.int64)
+  def __init__(self, *args):
+    super(ZeroRNodeClassifier, self).__init__(*args)
+    # The one-hot prediction.
+    self.r = np.zeros(self.labels_dimensionality)
+    self.r[0] = 1
 
-  @property
-  def y(self) -> np.array:
-    """Return the prediction array."""
-    a = np.zeros(self.y_dimensionality, dtype=np.int64)
-    a[np.argmax(self.class_counts)] = 1
-    return a
-
-  def MakeBatch(
-    self,
-    epoch_type: epoch.Type,
-    graphs: Iterable[graph_tuple_database.GraphTuple],
-    ctx: progress.ProgressContext = progress.NullContext,
-  ) -> batchs.Data:
-    del epoch_type  # Unused.
-    del ctx  # Unused.
-
-    batch_size = 0
-    graph_ids = []
-    targets = []
-
-    # Limit batch size to 10 million elements.
-    while batch_size < FLAGS.zero_r_batch_size:
-      # Read the next graph.
-      try:
-        graph = next(graphs)
-      except StopIteration:
-        # We have run out of graphs.
-        if len(graph_ids) == 0:
-          return batchs.EndOfBatches()
-        break
-
-      # Add the graph data to the batch.
-      graph_ids.append(graph.id)
-      if self.graph_db.node_y_dimensionality:
-        batch_size += graph.tuple.node_y.size
-        targets.append(graph.tuple.node_y)
+  def MakeMinibatchIterator(
+      self, epoch_type: str
+  ) -> typing.Iterable[typing.Tuple[log_database.BatchLog, np.array]]:
+    for batch in self.batcher.MakeGroupBatchIterator(epoch_type):
+      if 'node_y' in batch:
+        targets = batch['node_y']
+      elif 'graph_y' in batch:
+        targets = batch['graph_y']
       else:
-        batch_size += graph.tuple.graph_y.size
-        targets.append(graph.tuple.graph_y)
+        raise ValueError("Only node or graph labels are supported")
+      yield batch['log'], targets
 
-    return batchs.Data(
-      graph_ids=graph_ids, data=np.vstack(targets) if targets else None
-    )
+  def RunMinibatch(self, epoch_type: str, targets: np.array
+                  ) -> classifier_base.ClassifierBase.MinibatchResults:
+    # "Training" step selects the most frequent label from the training set.
+    if epoch_type == "train":
+      self.r = scipy.stats.mode(targets)[0]
 
-  def RunBatch(
-    self,
-    epoch_type: epoch.Type,
-    batch: batchs.Data,
-    ctx: progress.ProgressContext = progress.NullContext,
-  ) -> batchs.Results:
-    """Run a batch.
+    n = len(targets)
+    return self.MinibatchResults(loss=0,
+                                 y_true_1hot=targets,
+                                 y_pred_1hot=np.tile(self.r, n).reshape(
+                                     n, self.labels_dimensionality))
 
-    Args:
-      epoch_type: The type of epoch.
-      batch: The batch data.
-      ctx: A logging context.
+  def ModelDataToSave(self) -> typing.Any:
+    return self.r
 
-    Returns:
-      A Results instance.
-    """
-    del ctx
-
-    time.sleep(0.05)
-    targets = batch.data
-
-    # "Training" step updates the class frequency counts.
-    if epoch_type == epoch.Type.TRAIN:
-      bincount = np.bincount(
-        np.argmax(targets, axis=1), minlength=self.y_dimensionality
-      )
-      self.class_counts += bincount
-
-      assert targets.shape[1] == self.y.shape[0]
-
-    # The 1-hot predicted value.
-    predictions = np.tile(self.y, targets.shape[0]).reshape(targets.shape)
-
-    return batchs.Results.Create(targets=targets, predictions=predictions)
-
-  def GetModelData(self) -> Any:
-    return self.class_counts
-
-  def LoadModelData(self, data_to_load: Any) -> None:
-    self.class_counts = data_to_load
+  def LoadModelData(self, data_to_load: typing.Any) -> None:
+    self.r = data_to_load
 
 
 def main():
   """Main entry point."""
-  run.Run(ZeroR)
+  classifier_base.Run(ZeroRNodeClassifier)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.Run(main)
