7574cc6bbe9cf479efe934997c1fe868dfa172f4
Chris Cummins <chrisc.101@gmail.com>
2019-11-01 04:26:12 +0000

    Update devmap script.
    [Exported from 29fde4c8f9e2d513bec5d560a19727bbef5c75b7]

--- deeplearning/ml4pl/graphs/labelled/devmap/make_devmap_dataset.py
+++ deeplearning/ml4pl/graphs/labelled/devmap/make_devmap_dataset.py
@@ -1,24 +1,29 @@
 """This module prepares a CPU/GPU OpenCL device-mapping dataset."""
-from typing import Iterable
-
 import numpy as np
 import pandas as pd
+import pathlib
 import sqlalchemy as sql
+import tempfile
+import typing
 
 from datasets.opencl.device_mapping import opencl_device_mapping_dataset
-from deeplearning.ml4pl.graphs import programl
-from deeplearning.ml4pl.graphs import programl_pb2
-from deeplearning.ml4pl.graphs.labelled import graph_tuple as graph_tuples
-from deeplearning.ml4pl.graphs.labelled import graph_tuple_database
-from deeplearning.ml4pl.graphs.unlabelled import unlabelled_graph_database
-from deeplearning.ml4pl.ir import ir_database
-from labm8.py import app
-from labm8.py import progress
-from labm8.py import sqlutil
+from deeplearning.ml4pl.graphs import graph_database
+from deeplearning.ncc.inst2vec import api as inst2vec
+from labm8 import app
+from labm8 import fs
+from labm8 import sqlutil
 
-app.DEFINE_string(
-  "gpu", None, "The gpu to use. One of: {amd_tahiti_7970,nvidia_gtx_960}"
-)
+
+app.DEFINE_database('input_db',
+                    graph_database.Database,
+                    None,
+                    'URL of database to read unlabelled graphs from.',
+                    must_exist=True)
+app.DEFINE_database('output_db', graph_database.Database,
+                    'sqlite:////var/phd/deeplearning/ml4pl/graphs.db',
+                    'URL of the database to write labelled graphs to.')
+app.DEFINE_string('gpu', None,
+                  'The gpu to use. One of: {amd_tahiti_7970,nvidia_gtx_960}')
 
 FLAGS = app.FLAGS
 
@@ -27,159 +32,120 @@ def MakeGpuDataFrame(df: pd.DataFrame, gpu: str):
   """Construct a labelled data frame for the given GPU, where the 'y' column
   indicates whether the GPU was faster than the CPU.
   """
-  cpu = "intel_core_i7_3820"
+  cpu = 'intel_core_i7_3820'
 
-  df["relpath"] = [
-    ":".join(
-      [
-        r["program:benchmark_suite_name"],
-        r["program:benchmark_name"],
-        r["program:opencl_kernel_name"],
-      ]
-    )
-    for _, r in df.iterrows()
+  df['relpath'] = [
+      ":".join([
+          r['program:benchmark_suite_name'], r['program:benchmark_name'],
+          r['program:opencl_kernel_name']
+      ]) for _, r in df.iterrows()
   ]
 
-  df["y"] = [
-    np.array([0, 1], dtype=np.int64)
-    if r[f"runtime:{gpu}"] < r[f"runtime:{cpu}"]
-    else np.array([1, 0], dtype=np.int64)
-    for _, r in df.iterrows()
+  df['y'] = [
+      np.array([0, 1], dtype=np.int32)
+      if r[f'runtime:{gpu}'] < r[f'runtime:{cpu}'] else np.array([1, 0],
+                                                                 dtype=np.int32)
+      for _, r in df.iterrows()
   ]
 
-  df.rename(
-    columns={
-      f"param:{gpu}:wgsize": "wgsize",
-      f"feature:{gpu}:transfer": "transfer",
-      f"runtime:{cpu}": "runtime_cpu",
-      f"runtime:{gpu}": "runtime_gpu",
-    },
-    inplace=True,
-  )
+  df.rename(columns={
+      f'param:{gpu}:wgsize': 'wgsize',
+      f'feature:{gpu}:transfer': 'transfer',
+      f'runtime:{cpu}': 'runtime_cpu',
+      f'runtime:{gpu}': 'runtime_gpu',
+  },
+            inplace=True)
 
-  return df[
-    [
-      "relpath",
-      "wgsize",
-      "transfer",
-      "y",
-      "runtime_cpu",
-      "runtime_gpu",
-      "data:dataset_name",
-      "program:opencl_src",
-    ]
-  ]
+  return df[[
+      'relpath',
+      'wgsize',
+      'transfer',
+      'y',
+      'runtime_cpu',
+      'runtime_gpu',
+      'data:dataset_name',
+  ]]
 
 
-def AnnotateGraphMetas(
-  ir_db: ir_database.Database,
-  proto_db: unlabelled_graph_database.Database,
-  df: pd.DataFrame,
-  ctx: progress.ProgressContext = progress.NullContext,
-) -> Iterable[graph_tuple_database.GraphTuple]:
-  """Add features and labels to graph metas in database."""
-  with ir_db.Session() as ir_session, proto_db.Session() as proto_session:
+def MakeAnnotatedGraphs(input_db: graph_database.Database, df: pd.DataFrame
+                       ) -> typing.Iterable[graph_database.GraphMeta]:
+  """Make annotated graph's for the given devmap dataset."""
+  with input_db.Session() as session:
     for _, row in df.iterrows():
-      relpath = row["relpath"]
-      with ctx.Profile(
-        2, f"Processed graph {row['relpath']}:{row['data:dataset_name']}"
-      ):
-        # Select the corresponding IR.
-        ir_id = (
-          ir_session.query(ir_database.IntermediateRepresentation.id)
-          .filter(
-            ir_database.IntermediateRepresentation.source
-            == "pact17_opencl_devmap",
-            ir_database.IntermediateRepresentation.relpath == relpath,
-          )
-          .scalar()
-        )
-        # Check that we have an exact 1:1 mapping from the opencl devmap dataset
-        # to IR.
-        if ir_id is None:
-          raise ValueError(f"Expected one IR with relpath {relpath}")
+      q = session.query(graph_database.GraphMeta)
 
-        # Load the program graph.
-        proto_row = (
-          proto_session.query(unlabelled_graph_database.ProgramGraph)
-          .filter(unlabelled_graph_database.ProgramGraph.ir_id == ir_id)
-          .options(
-            sql.orm.joinedload(unlabelled_graph_database.ProgramGraph.data)
-          )
-          .scalar()
-        )
-        if proto_row is None:
-          raise ValueError(
-            f"Expected one proto for relpath {relpath} with ID {ir_id}"
-          )
-        proto: programl_pb2.ProgramGraph = proto_row.proto
+      # Select the corresponding graph from the input database.
+      q = q.filter(
+          graph_database.GraphMeta.source_name == 'pact17_opencl_devmap')
+      q = q.filter(graph_database.GraphMeta.relpath == row['relpath'])
 
-        # Add the null "selector vector" value.
-        for node in proto.node:
-          node.x.append(0)
+      # Check that we have an exact 1:1 mapping from the opencl devmap dataset
+      # to graphs in the input database.
+      if q.count() != 1:
+        app.Error("Expected one graph with relpath %s, but found %s",
+                  row['relpath'], q.count())
+        continue
 
-        # Add the graph-level features.
-        proto.x[:] = [row["wgsize"], row["transfer"]]
-        # Add 'y' graph feature as target.
-        proto.y[:] = row["y"].tolist()
+      # Load the graph data.
+      q = q.options(sql.orm.joinedload(graph_database.GraphMeta.graph))
+      input_graph_meta = q.first()
+      graph = input_graph_meta.data
 
-        # Create the graph tuple. Note the jumping through hoops with converting
-        # proto -> nx -> graph_tuple, because there is currently no direct
-        # proto -> graph_tuple conversion.
-        graph_tuple = graph_tuple_database.GraphTuple.CreateFromGraphTuple(
-          graph_tuple=graph_tuples.GraphTuple.CreateFromNetworkX(
-            programl.ProgramGraphToNetworkX(proto)
-          ),
-          ir_id=ir_id,
-        )
-      yield graph_tuple
+      # Add the graph-level features.
+      # TODO(cec): Should we apply any transforms to these values? Log?
+      graph.x = np.array([row['wgsize'], row['transfer']], dtype=np.int64)
+
+      # Add 'y' graph feature as target.
+      graph.y = row['y']
+
+      # Add graph metadata.
+      graph.group = input_graph_meta.group
+      graph.name = ':'.join([row['relpath'], row['data:dataset_name']])
+      graph.runtime_cpu = row['runtime_cpu']
+      graph.runtime_gpu = row['runtime_gpu']
+
+      yield graph
 
 
-class MakeOpenClDevmapDataset(progress.Progress):
+def MakeOpenClDevmapDataset(input_db: graph_database.Database,
+                            output_db: graph_database.Database, gpu: str):
   """Create a labelled dataset for the given GPU."""
+  # TODO(cec): Change groups to k-fold classification.
+  dataset = opencl_device_mapping_dataset.OpenClDeviceMappingsDataset()
 
-  def __init__(
-    self,
-    ir_db: ir_database.Database,
-    proto_db: unlabelled_graph_database.Database,
-    graph_db: graph_tuple_database.Database,
-    gpu: str,
-  ):
-    self.ir_db = ir_db
-    self.proto_db = proto_db
-    self.graph_db = graph_db
-    self.gpu = gpu
+  with sqlutil.BufferedDatabaseWriter(output_db,
+                                      max_queue=8).Session() as writer:
+    df = MakeGpuDataFrame(dataset.df, gpu)
 
-    self.dataset = opencl_device_mapping_dataset.OpenClDeviceMappingsDataset()
-    super(MakeOpenClDevmapDataset, self).__init__(
-      gpu, i=0, n=len(self.dataset.df), unit="protos"
-    )
-
-  def Run(self):
-    with sqlutil.BufferedDatabaseWriter(
-      self.graph_db, max_buffer_size=32 * 1024 * 1024, max_buffer_length=1024
-    ) as writer:
-      df = MakeGpuDataFrame(self.dataset.df, self.gpu)
-
-      for graph in AnnotateGraphMetas(
-        self.ir_db, self.proto_db, df, ctx=self.ctx
-      ):
-        self.ctx.i += 1
-        writer.AddOne(graph)
+    for graph in MakeAnnotatedGraphs(input_db, df):
+      app.Log(1, 'Processed %s', graph.name)
+      writer.AddOne(
+          graph_database.GraphMeta.CreateFromNetworkX(
+              graph, edge_types={'control', 'data', 'call'},
 
 
 def main():
   """Main entry point."""
-  ir_db = FLAGS.ir_db()
-  proto_db = FLAGS.proto_db()
-  graph_db = FLAGS.graph_db()
+  input_db = FLAGS.input_db()
+  output_db = FLAGS.output_db()
   gpu = FLAGS.gpu
 
-  if gpu not in {"amd_tahiti_7970", "nvidia_gtx_960"}:
+  if gpu not in {'amd_tahiti_7970', 'nvidia_gtx_960'}:
     raise app.UsageError("Unknown GPU")
 
-  progress.Run(MakeOpenClDevmapDataset(ir_db, proto_db, graph_db, gpu))
+  # Temporarily redirect logs to a file, which we will later import into the
+  # database's meta table.
+  with tempfile.TemporaryDirectory() as d:
+    app.LogToDirectory(d, 'log')
+
+    MakeOpenClDevmapDataset(input_db, output_db, gpu)
+
+    log = fs.Read(pathlib.Path(d) / 'log.INFO')
+    with output_db.Session(commit=True) as s:
+      s.query(graph_database.Meta).filter(
+          graph_database.Meta.key == 'log').delete()
+      s.add(graph_database.Meta(key='log', value=log))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.Run(main)
