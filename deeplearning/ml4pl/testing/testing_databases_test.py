0302f80351bbb5ad4f768bba1b0edcb4da4ccf4f
Chris Cummins <chrisc.101@gmail.com>
2019-11-29 21:24:20 +0000

    Add utility module to enable MySQL test fixtures.
    [Exported from b3dc0df59623e8e99ba627dadd74633be8a7f8b5]

--- deeplearning/ml4pl/testing/testing_databases_test.py
+++ deeplearning/ml4pl/testing/testing_databases_test.py
@@ -1,25 +1,9 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Unit tests for //deeplearning/ml4pl/test:testing_database."""
 import sqlalchemy as sql
 
 from deeplearning.ml4pl.testing import testing_databases
 from labm8.py import sqlutil
 from labm8.py import test
-
 
 FLAGS = test.FLAGS
 
@@ -42,11 +26,7 @@ class Database(sqlutil.Database):
     super(Database, self).__init__(url, Base, must_exist=must_exist)
 
 
-@test.Fixture(
-  scope="function",
-  params=testing_databases.GetDatabaseUrls(),
-  namer=testing_databases.DatabaseUrlNamer("db"),
-)
+@test.Fixture(scope="function", params=testing_databases.TEST_DB_URLS)
 def empty_db(request) -> Database:
   """A test fixture which yields an empty database."""
   yield from testing_databases.YieldDatabase(Database, request.param)
@@ -58,11 +38,7 @@ def test_YieldDatabase_fixture(empty_db: Database):
     assert session.query(Table).first() is None
 
 
-@test.Fixture(
-  scope="function",
-  params=testing_databases.GetDatabaseUrls(),
-  namer=testing_databases.DatabaseUrlNamer("db"),
-)
+@test.Fixture(scope="function", params=testing_databases.TEST_DB_URLS)
 def populated_db(request) -> Database:
   """A test fixture which yields an empty database."""
   with testing_databases.DatabaseContext(Database, request.param) as db:
