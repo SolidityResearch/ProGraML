4290aeffaaeb3593806424aadee1846bc143594c
Chris Cummins <chrisc.101@gmail.com>
2019-12-02 01:24:44 +0000

    Add a module for generating random nx graphs.
    github.com/ChrisCummins/ProGraML/issues/23
    
    [Exported from 8e9c388940453c31ebd9cc328954d263add60439]

--- deeplearning/ml4pl/testing/random_networkx_generator.py
+++ deeplearning/ml4pl/testing/random_networkx_generator.py
@@ -1,28 +1,12 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """This module defines a generator for random graph tuples."""
+import functools
 from typing import Iterable
-from typing import Optional
 
 import networkx as nx
 
 from deeplearning.ml4pl.graphs import programl
 from deeplearning.ml4pl.testing import random_programl_generator
 from labm8.py import test
-
 
 FLAGS = test.FLAGS
 
@@ -32,7 +16,6 @@ def CreateRandomGraph(
   node_y_dimensionality: int = 0,
   graph_x_dimensionality: int = 0,
   graph_y_dimensionality: int = 0,
-  node_count: int = None,
 ) -> nx.MultiDiGraph:
   """Generate a random graph.
 
@@ -52,12 +35,12 @@ def CreateRandomGraph(
     node_y_dimensionality=node_y_dimensionality,
     graph_x_dimensionality=graph_x_dimensionality,
     graph_y_dimensionality=graph_y_dimensionality,
-    node_count=node_count,
   )
   return programl.ProgramGraphToNetworkX(proto)
 
 
-def EnumerateTestSet(n: Optional[int] = None) -> Iterable[nx.MultiDiGraph]:
+@functools.lru_cache(maxsize=2)
+def EnumerateGraphTestSet() -> Iterable[nx.MultiDiGraph]:
   """Enumerate a test set of "real" graphs."""
-  for proto in random_programl_generator.EnumerateTestSet(n=n):
+  for proto in random_programl_generator.EnumerateProtoTestSet():
     yield programl.ProgramGraphToNetworkX(proto)
--- deeplearning/ml4pl/testing/random_networkx_generator_test.py
+++ deeplearning/ml4pl/testing/random_networkx_generator_test.py
@@ -1,32 +1,9 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Unit tests for //deeplearning/ml4pl/testing:random_networkx_generator."""
 from deeplearning.ml4pl.testing import random_networkx_generator
 from labm8.py import decorators
 from labm8.py import test
 
 FLAGS = test.FLAGS
-
-
-@decorators.loop_for(seconds=2)
-@test.Parametrize("node_count", (5, 10, 20))
-def test_CreateRandomGraph_node_count(node_count: int):
-  """Test generating protos with specific node counts."""
-  g = random_networkx_generator.CreateRandomGraph(node_count=node_count)
-  assert g.number_of_nodes() == node_count
 
 
 @decorators.loop_for(seconds=2)
@@ -47,16 +24,15 @@ def test_CreateRandomGraph(
     graph_x_dimensionality=graph_x_dimensionality,
     graph_y_dimensionality=graph_y_dimensionality,
   )
-  for _, data in g.nodes(data=True):
-    assert len(data["x"]) == node_x_dimensionality
-    assert len(data["y"]) == node_y_dimensionality
+  assert len(g.nodes[0]["x"]) == node_x_dimensionality
+  assert len(g.nodes[0]["y"]) == node_y_dimensionality
   assert len(g.graph["x"]) == graph_x_dimensionality
   assert len(g.graph["y"]) == graph_y_dimensionality
 
 
-def test_EnumerateTestSet():
+def test_EnumerateGraphTestSet():
   """Test the "real" protos."""
-  protos = list(random_networkx_generator.EnumerateTestSet())
+  protos = list(random_networkx_generator.EnumerateGraphTestSet())
   assert len(protos) == 100
 
 
