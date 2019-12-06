0530bbb252c001dedb5075441391bf7ec710a9fb
Chris Cummins <chrisc.101@gmail.com>
2019-12-06 11:55:11 +0000

    Propagate the 'default' arg of json.dumps().
    [Exported from ba342441cd580952e8e4b895a2c3bcd130caae60]

--- labm8/py/jsonutil.py
+++ labm8/py/jsonutil.py
@@ -17,16 +17,7 @@ import json
 import re
 import typing
 
-<<<<<<< HEAD:labm8/py/jsonutil.py
-from phd.lib.labm8 import fs
-
-<<<<<<< HEAD:labm8/py/jsonutil.py
 from labm8.py import fs
-=======
->>>>>>> 386c66354... Add 'phd' prefix to labm8 imports.:lib/labm8/jsonutil.py
-=======
-from labm8.py import fs
->>>>>>> 8be094257... Move //labm8 to //labm8/py.:labm8/py/jsonutil.py
 
 # A type alias for annotating methods which take or return JSON.
 JSON = typing.Union[typing.List[typing.Any], typing.Dict[str, typing.Any]]
@@ -42,13 +33,9 @@ def format_json(data, default=None):
   Returns:
       str: Formatted JSON
   """
-<<<<<<< HEAD
   return json.dumps(
     data, sort_keys=True, indent=2, separators=(",", ": "), default=default
   )
-=======
-  return json.dumps(data, sort_keys=True, indent=2, separators=(",", ": "))
->>>>>>> 4242aed2a... Automated code format.
 
 
 def read_file(*components, **kwargs):
@@ -79,23 +66,10 @@ def read_file(*components, **kwargs):
       return loads(infile.read())
   except ValueError as e:
     raise ValueError(
-<<<<<<< HEAD
-<<<<<<< HEAD:labm8/py/jsonutil.py
-=======
->>>>>>> 4242aed2a... Automated code format.
       "malformed JSON file '{path}'. Message from parser: {err}".format(
         path=fs.basename(path), err=str(e),
       ),
     )
-<<<<<<< HEAD
-=======
-        "malformed JSON file '{path}'. Message from parser: {err}".format(
-            path=fs.basename(path),
-            err=str(e),
-        ),)
->>>>>>> 49340dc00... Auto-format labm8 python files.:labm8/jsonutil.py
-=======
->>>>>>> 4242aed2a... Automated code format.
   except IOError as e:
     if not must_exist:
       return {}
@@ -147,37 +121,14 @@ def loads(text, **kwargs):
 
   for index, line in enumerate(lines):
     if re.search(regex, line):
-<<<<<<< HEAD
-<<<<<<< HEAD:labm8/py/jsonutil.py
-      if re.search(r"^" + regex, line, re.IGNORECASE):
-        lines[index] = ""
-=======
-      if re.search(r'^' + regex, line, re.IGNORECASE):
-        lines[index] = ''
->>>>>>> 49340dc00... Auto-format labm8 python files.:labm8/jsonutil.py
-      elif re.search(regex_inline, line):
-        lines[index] = re.sub(regex_inline, r"\1", line)
-
-  return json.loads("\n".join(lines), **kwargs)
-
-
-<<<<<<< HEAD:labm8/py/jsonutil.py
-=======
-  return json.loads('\n'.join(lines), **kwargs)
-=======
       if re.search(r"^" + regex, line, re.IGNORECASE):
         lines[index] = ""
       elif re.search(regex_inline, line):
         lines[index] = re.sub(regex_inline, r"\1", line)
 
   return json.loads("\n".join(lines), **kwargs)
->>>>>>> 4242aed2a... Automated code format.
 
-<<<<<<< HEAD:labm8/py/jsonutil.py
->>>>>>> 4f357866c... Add two utility functions.:labm8/jsonutil.py
-=======
 
->>>>>>> 8be094257... Move //labm8 to //labm8/py.:labm8/py/jsonutil.py
 def JsonSerializable(val):
   """Return a JSON-serializable version of the object.
 
