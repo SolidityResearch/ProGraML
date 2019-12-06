c52d93ae03812c115b7d57c603a765bde1c55cfa
Chris Cummins <chrisc.101@gmail.com>
2019-12-06 20:39:30 +0000

    Add a module for scheduling enums.
    [Exported from d79e048157539b8cb8052b23b106d4c0eb8b2105]

--- deeplearning/ml4pl/models/schedules.py
+++ deeplearning/ml4pl/models/schedules.py
@@ -7,28 +7,19 @@ from labm8.py import app
 FLAGS = app.FLAGS
 
 
-class SaveOn(enum.Enum):
-  """Determine when to save checkpoints."""
-
-  # Never save a checkpoint.
-  NONE = 0
-  # Make a checkpoint at every epoch.
-  EVERY_EPOCH = 1
-  # Make a checkpoint when validation accuracy improves.
-  VAL_IMPROVED = 2
-
-
 class KeepCheckpoints(enum.Enum):
-  """Determine how many checkpoints to keep."""
+  """Schedules for keeping checkpoint logs."""
 
+  # Keep no checkpoints.
+  NONE = 0
   # Keep all checkpoints.
   ALL = 1
-  # Keep only the last saved checkpoint.
-  LAST = 2
+  # Keep the checkpoint for the most recent epoch.
+  LAST_EPOCH = 2
 
 
 class KeepDetailedBatches(enum.Enum):
-  """Determine how many detailed batches to keep."""
+  """Schedules for keeping detailed batch logs."""
 
   # Keep no detailed batches.
   NONE = 0
