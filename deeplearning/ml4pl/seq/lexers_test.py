9aa84234742500413c1db4a6af4dcafa7d03f2e0
Chris Cummins <chrisc.101@gmail.com>
2019-12-07 01:34:14 +0000

    Add a lexer module.
    github.com/ChrisCummins/ProGraML/issues/24
    
    [Exported from 75f2ad60b832f7dc15d965bfc1834370fb11a8d4]

--- deeplearning/ml4pl/seq/lexers_test.py
+++ deeplearning/ml4pl/seq/lexers_test.py
@@ -1,18 +1,3 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Unit tests for //deeplearning/ml4pl/seq:lexer."""
 import random
 import string
@@ -24,7 +9,6 @@ from deeplearning.ml4pl.seq import lexers
 from labm8.py import decorators
 from labm8.py import test
 
-
 FLAGS = test.FLAGS
 
 
@@ -34,29 +18,27 @@ def lexer_type(request) -> lexers.LexerType:
   return request.param
 
 
-@test.Fixture(scope="function", params=({"abc": 0, "bcd": 1},))
-def vocabulary(request) -> Dict[str, int]:
+@test.Fixture(scope="function", params=({}, {"abc": 0, "bcd": 1}))
+def initial_vocab(request) -> Dict[str, int]:
   """Test fixture for initial vocabs."""
   return request.param
 
 
 @test.Fixture(scope="function", params=(10, 1024, 1024 * 1024))
-def max_encoded_length(request) -> int:
-  """Test fixture for lexer max encoded lengths."""
+def max_chunk_size(request) -> int:
+  """Test fixture for lexer max chunk sizes."""
   return request.param
 
 
 @test.Fixture(scope="function")
 def lexer(
   lexer_type: lexers.LexerType,
-  vocabulary: Dict[str, int],
-  max_encoded_length: int,
+  initial_vocab: Dict[str, int],
+  max_chunk_size: int,
 ) -> lexers.Lexer:
   """A test fixture which returns a lexer."""
   return lexers.Lexer(
-    type=lexer_type,
-    vocabulary=vocabulary,
-    max_encoded_length=max_encoded_length,
+    type=lexer_type, initial_vocab=initial_vocab, max_chunksize=max_chunk_size
   )
 
 
@@ -69,16 +51,31 @@ def CreateRandomString(min_length: int = 1, max_length: int = 1024) -> str:
 
 
 @decorators.loop_for(seconds=30)
-def test_fuzz_Lex(lexer: lexers.Lexer, max_encoded_length: int):
+def test_fuzz_Lex(lexer: lexers.Lexer):
   """Fuzz the lexer."""
   texts_count = random.randint(1, 128)
   texts = [CreateRandomString() for _ in range(texts_count)]
 
-  lexed = lexer.Lex(texts)
-  assert len(lexed) == texts_count
-  for encoded in lexed:
-    assert len(encoded) <= max_encoded_length
-    assert not np.where(encoded > lexer.vocabulary_size)[0].size
+  initial_vocab_size = len(lexer.vocab)
+
+  encodeds = lexer.Lex(texts)
+  assert len(lexer.vocab) == initial_vocab_size
+  assert len(encodeds) == texts_count
+  for encoded in encodeds:
+    assert not np.where(encoded > initial_vocab_size + 1)[0].size
+
+
+@decorators.loop_for(seconds=30)
+def test_fuzz_LexAndUpdateVocab(lexer: lexers.Lexer):
+  """Fuzz the lexer."""
+  texts_count = random.randint(1, 128)
+  texts = [CreateRandomString() for _ in range(texts_count)]
+
+  initial_vocab_size = len(lexer.vocab)
+
+  encodeds = lexer.LexAndUpdateVocab(texts)
+  assert len(lexer.vocab) >= initial_vocab_size
+  assert len(encodeds) == texts_count
 
 
 if __name__ == "__main__":
