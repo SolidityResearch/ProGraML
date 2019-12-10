073829fe3e6993be25a75ff177ef555f37220635
Chris Cummins <chrisc.101@gmail.com>
2019-12-10 15:35:52 +0000

    Add an annotator binary.
    [Exported from c5cdac289d973a2126a026a16359c5555512921f]

--- deeplearning/ml4pl/graphs/labelled/dataflow/annotate.py
+++ deeplearning/ml4pl/graphs/labelled/dataflow/annotate.py
@@ -23,13 +23,13 @@ and text format protocol buffers:
         --n=5 \
         < /tmp/program_graph.pbtxt
 """
-import signal
 import subprocess
-import sys
+import time
 from typing import Callable
 from typing import Dict
+from typing import Iterable
 from typing import List
-from typing import Union
+from typing import Optional
 
 from deeplearning.ml4pl.graphs import programl
 from deeplearning.ml4pl.graphs import programl_pb2
@@ -49,45 +49,22 @@ from deeplearning.ml4pl.testing import test_annotators
 from labm8.py import app
 from labm8.py import bazelutil
 
-app.DEFINE_boolean(
-  "list", False, "If true, list the available analyses and exit."
-)
-app.DEFINE_string("analysis", "", "The name of the analysis to run.")
-app.DEFINE_integer(
-  "n",
-  10,
-  "The maximum number of labelled program graphs to produce. "
-  "For a graph with `n` root statements, `n` instances can be produced by "
-  "changing the root statement. If --n=0, enumerate all possible labelled "
-  "graphs.",
-)
 
-FLAGS = app.FLAGS
+class TimeoutAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
+  def MakeAnnotated(
+    self, unlabelled_graph: programl_pb2.ProgramGraph, n: Optional[int] = None
+  ) -> Iterable[programl_pb2.ProgramGraph]:
+    time.sleep(int(1e6))
+
 
 # A map from analysis name to a callback which instantiates a
 # DataFlowGraphAnnotator object for this anlysis. To add a new analysis, create
 # a new entry in this table.
-ANALYSES = {
-  "reachability": reachability.ReachabilityAnnotator,
-  "domtree": dominator_tree.DominatorTreeAnnotator,
-  "liveness": liveness.LivenessAnnotator,
-  "datadep": data_dependence.DataDependencyAnnotator,
-  "subexpressions": subexpressions.CommonSubexpressionAnnotator,
-  # Annotators which are used for testing this script. These should, for obvious
-  # reasons, not be used in prod. However, they must remain here so that we can
-  # test the behaviour of the annotator under various conditions.
-  "test_pass_thru": test_annotators.PassThruAnnotator,
-  "test_flaky": test_annotators.FlakyAnnotator,
-  "test_timeout": test_annotators.TimeoutAnnotator,
-  "test_error": test_annotators.ErrorAnnotator,
-  "test_empty": test_annotators.EmptyAnnotator,
+ANALYSES: Dict[str, Callable[[], data_flow_graphs.DataFlowGraphAnnotator]] = {
+  "reachability": lambda: reachability.ReachabilityAnnotator(),
+  # Annotators which are used for testing this script:
+  "test_timeout": lambda: test_annotators.TimeoutAnnotator(),
 }
-
-# A list of the available analyses. We filter out the test_xxx named annotators
-# for clarity.
-AVAILABLE_ANALYSES = sorted(
-  analysis for analysis in ANALYSES if not analysis.startswith("test_")
-)
 
 # The path of this script. Because a target cannot depend on itself, all calling
 # code must add this script to its `data` dependencies.
@@ -95,9 +72,23 @@ SELF = bazelutil.DataPath(
   "phd/deeplearning/ml4pl/graphs/labelled/dataflow/annotate"
 )
 
+app.DEFINE_boolean(
+  "list", False, "If true, list the available analyses and exit."
+)
+app.DEFINE_string("analysis", "", "The name of the analysis to run.")
+app.DEFINE_integer(
+  "n",
+  0,
+  "The maximum number of labelled program graphs to produce. "
+  "For a graph with `n` root statements, `n` instances can be produced by "
+  "changing the root statement. If --n=0, enumerate all possible labelled "
+  "graphs.",
+)
 
 # Return codes for error conditions.
 #
+# Error initializing the requested analysis.
+E_ANALYSIS_INIT = 10
 # Error reading stdin.
 E_INVALID_INPUT = 11
 # The analysis failed.
@@ -105,18 +96,41 @@ E_ANALYSIS_FAILED = 12
 # Error writing stdout.
 E_INVALID_STDOUT = 13
 
+FLAGS = app.FLAGS
 
-def _AnnotateInSubprocess(
+
+class AnalysisFailed(OSError):
+  """An error raised if the analysis failed."""
+
+  def __init__(self, returncode: int, stderr: str):
+    self.returncode = returncode
+    self.stderr = stderr
+
+  def __repr__(self):
+    return {
+      E_ANALYSIS_INIT: "Analysis failed to initialize",
+      E_INVALID_INPUT: "Analysis failed to read stdin",
+      E_ANALYSIS_FAILED: "Analysis failed",
+      E_INVALID_STDOUT: "Analysis failed to write stdout",
+    }.get(self.returncode, "Unknown error")
+
+
+class AnalysisTimeout(AnalysisFailed):
+  def __init__(self, returncode: int, stderr: str, timeout: int):
+    super(AnalysisTimeout, self).__init__(returncode, stderr)
+    self.timeout = timeout
+
+  def __repr__(self):
+    return f"Analysis failed to complete within {self.timeout} seconds"
+
+
+def Annotate(
   analysis: str,
-  graph: Union[programl_pb2.ProgramGraph, bytes],
+  graph: programl_pb2.ProgramGraph,
   n: int = 0,
   timeout: int = 120,
-  binary_graph: bool = False,
 ) -> programl_pb2.ProgramGraphs:
-  """Run this script in a subprocess.
-
-  This is the most robust method for enforcing the timeout, but has a huge
-  overhead in starting up a new python interpreter for every invocation.
+  """Programatically run this script and return the output.
 
   DISCLAIMER: Because a target cannot depend on itself, all calling code must
   add //deeplearning/ml4pl/graphs/labelled/dataflow:annotate to its list of
@@ -124,21 +138,15 @@ def _AnnotateInSubprocess(
 
   Args:
     analysis: The name of the analysis to run.
-    graph: The unlabelled ProgramGraph protocol buffer to to annotate, either
-      as a proto instance or as binary-encoded byte array.
+    graph: The unlabelled graph to annotate.
     n: The maximum number of labelled graphs to produce.
     timeout: The maximum number of seconds to run the analysis for.
-    binary_graph: If true, treat the graph argument as a binary byte array.
 
   Returns:
     A ProgramGraphs protocol buffer.
 
   Raises:
-    IOError: If serializing the input or output protos fails.
-    ValueError: If an invalid analysis is requested.
-    data_flow_graphs.AnalysisFailed: If the analysis raised an error.
-    data_flow_graphs.AnalysisTimeout: If the analysis did not complete within
-      the requested timeout.
+    AnalysisFailed: If the analysis script raised an error.
   """
   process = subprocess.Popen(
     [
@@ -160,119 +168,66 @@ def _AnnotateInSubprocess(
     stderr=subprocess.PIPE,
   )
 
-  # Encode the input if required.
-  if binary_graph:
-    stdin = graph
-  else:
-    stdin = programl.ToBytes(graph, fmt=programl.InputOutputFormat.PB)
-
-  # Run this analysis script.
-  stdout, stderr = process.communicate(stdin)
-
+  stdout, stderr = process.communicate(
+    programl.ToBytes(graph, fmt=programl.InputOutputFormat.PB)
+  )
   if process.returncode == 9 or process.returncode == -9:
     # Process was killed. We assume this is because of timeout, though it could
     # be the user.
-    raise data_flow_graphs.AnalysisTimeout(timeout)
-  elif process.returncode == E_INVALID_INPUT:
-    raise IOError("Failed to serialize input graph")
-  elif process.returncode == E_INVALID_STDOUT:
-    raise IOError("Analysis failed to write stdout")
+    raise AnalysisTimeout(process.returncode, stderr, timeout)
   elif process.returncode:
-    raise data_flow_graphs.AnalysisFailed(
-      f"Analysis failed with returncode {process.returncode}: "
-      f"{stderr.decode('utf-8')}"
-    )
+    raise AnalysisFailed(process.returncode, stderr)
 
   # Construct the protocol buffer from stdout.
-  output = programl.FromBytes(
+  return programl.FromBytes(
     stdout,
     programl.InputOutputFormat.PB,
     proto=programl_pb2.ProgramGraphs(),
     empty_okay=True,
   )
 
-  return output
-
-
-def Annotate(
-  analysis: str,
-  graph: Union[programl_pb2.ProgramGraph, bytes],
-  n: int = 0,
-  timeout: int = 120,
-) -> data_flow_graphs.DataFlowGraphs:
-  """Run an analysis
-
-  Args:
-    analysis: The name of the analysis to run.
-    graph: The unlabelled ProgramGraph protocol buffer to to annotate, either
-      as a proto instance or as binary-encoded byte array.
-    n: The maximum number of labelled graphs to produce.
-    timeout: The maximum number of seconds to run the analysis for.
-    binary_graph: If true, treat the graph argument as a binary byte array.
-
-  Returns:
-    A ProgramGraphs protocol buffer.
-
-  Raises:
-    ValueError: If an invalid analysis is requested.
-    IOError: If serializing the input or output protos fails.
-    data_flow_graphs.AnalysisFailed: If the analysis raised an error.
-    data_flow_graphs.AnalysisTimeout: If the analysis did not complete within
-      the requested timeout.
-  """
-  if analysis not in ANALYSES:
-    raise ValueError(
-      f"Unknown analysis: {analysis}. "
-      f"Available analyses: {AVAILABLE_ANALYSES}",
-    )
-
-  def TimeoutHandler(signum, frame):
-    """Callback to raise a timeout error."""
-    del signum
-    del frame
-    raise data_flow_graphs.AnalysisTimeout(timeout)
-
-  signal.signal(signal.SIGALRM, TimeoutHandler)
-  signal.alarm(timeout)
-  annotator = ANALYSES[analysis](graph)
-
-  try:
-    annotated_graphs = annotator.MakeAnnotated(n)
-  finally:
-    signal.alarm(0)
-
-  return annotated_graphs
-
 
 def Main():
   """Main entry point."""
   if FLAGS.list:
-    print(f"Available analyses: {AVAILABLE_ANALYSES}")
+    print(f"Available analyses: {sorted(ANALYSES.keys())}")
     return
 
+  try:
+    annotator = ANALYSES.get(FLAGS.analysis, lambda: None)()
+    if not annotator:
+      raise app.UsageError(
+        f"Unknown analysis: {FLAGS.analysis}. "
+        f"Available analyses: {sorted(ANALYSES.keys())}"
+      )
+  except Exception as e:
+    app.FatalWithoutStackTrace(
+      "Error initializing analysis: %s", e, returncode=E_ANALYSIS_INIT
+    )
   n = FLAGS.n
 
   try:
     input_graph = programl.ReadStdin()
   except Exception as e:
-    print(f"Error parsing stdin: {e}", file=sys.stderr)
-    sys.exit(E_INVALID_INPUT)
-
-  annotator = ANALYSES[FLAGS.analysis](input_graph)
+    app.FatalWithoutStackTrace(
+      "Error parsing stdin: %s", e, returncode=E_INVALID_INPUT
+    )
 
   annotated_graphs: List[programl_pb2.ProgramGraph] = []
   try:
-    for annotated_graph in annotator.MakeAnnotated(n):
+    for annotated_graph in annotator.MakeAnnotated(input_graph, n):
       annotated_graphs.append(annotated_graph)
   except Exception as e:
-    print(f"{e}", file=sys.stderr)
-    sys.exit(E_ANALYSIS_FAILED)
+    app.FatalWithoutStackTrace(
+      "Error during analysis: %s", e, returncode=E_ANALYSIS_FAILED
+    )
 
   try:
     programl.WriteStdout(programl_pb2.ProgramGraphs(graph=annotated_graphs))
   except Exception as e:
-    print(f"Error writing stdout: {e}", file=sys.stderr)
-    sys.exit(E_INVALID_STDOUT)
+    app.FatalWithoutStackTrace(
+      "Error writing stdout: %s", e, returncode=E_INVALID_STDOUT
+    )
 
 
 if __name__ == "__main__":
--- deeplearning/ml4pl/testing/test_annotators.py
+++ deeplearning/ml4pl/testing/test_annotators.py
@@ -1,9 +1,8 @@
 """Data flow analyses for testing."""
-import random
 import time
+from typing import Iterable
 from typing import Optional
 
-from deeplearning.ml4pl.graphs import programl
 from deeplearning.ml4pl.graphs import programl_pb2
 from deeplearning.ml4pl.graphs.labelled.dataflow import data_flow_graphs
 from labm8.py import app
@@ -12,56 +11,14 @@ from labm8.py import app
 FLAGS = app.FLAGS
 
 
-class PassThruAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis that returns its input."""
-
-  def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    g = programl.ProgramGraphToNetworkX(self.unlabelled_graph)
-    return data_flow_graphs.NetworkxDataFlowGraphs([g for _ in range(n or 1)])
-
-
-class FlakyAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis that sometimes fails, and sometimes returns `n` graphs."""
-
-  def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    if random.random() < 0.2:
-      raise OSError("something went wrong!")
-
-    g = programl.ProgramGraphToNetworkX(self.unlabelled_graph)
-    return data_flow_graphs.NetworkxDataFlowGraphs([g for _ in range(n or 1)])
-
-
 class TimeoutAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
   """An analysis that times out."""
 
-  def __init__(self, *args, seconds: int = int(1e6), **kwargs):
-    super(TimeoutAnnotator, self).__init__(*args, **kwargs)
+  def __init__(self, seconds: int = int(1e6)):
     self.seconds = seconds
 
   def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
+    self, unlabelled_graph: programl_pb2.ProgramGraph, n: Optional[int] = None
+  ) -> Iterable[programl_pb2.ProgramGraph]:
     time.sleep(self.seconds)
-    return data_flow_graphs.NetworkxDataFlowGraphs([])
-
-
-class ErrorAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis that raises an error."""
-
-  def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    raise OSError("something went wrong!")
-
-
-class EmptyAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis which produces no outputs."""
-
-  def MakeAnnotated(
-    self, n: int = 0
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    return data_flow_graphs.NetworkxDataFlowGraphs([])
+    yield programl_pb2.ProgramGraph()
