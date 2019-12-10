0ebf0fe162aca5f8a29c0dbcb8427a50683587d9
Chris Cummins <chrisc.101@gmail.com>
2019-12-10 15:35:52 +0000

    Add an annotator binary.
    [Exported from c5cdac289d973a2126a026a16359c5555512921f]

--- deeplearning/ml4pl/testing/test_annotators.py
+++ deeplearning/ml4pl/testing/test_annotators.py
@@ -1,24 +1,9 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Data flow analyses for testing."""
-import random
 import time
+from typing import Iterable
 from typing import Optional
 
-from deeplearning.ml4pl.graphs import programl
+from deeplearning.ml4pl.graphs import programl_pb2
 from deeplearning.ml4pl.graphs.labelled.dataflow import data_flow_graphs
 from labm8.py import app
 
@@ -26,56 +11,14 @@ from labm8.py import app
 FLAGS = app.FLAGS
 
 
-class PassThruAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis that returns its input."""
-
-  def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    g = programl.ProgramGraphToNetworkX(self.unlabelled_graph)
-    return data_flow_graphs.NetworkxDataFlowGraphs([g for _ in range(n or 1)])
-
-
-class FlakyAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis that sometimes fails, and sometimes returns `n` graphs."""
-
-  def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    if random.random() < 0.2:
-      raise OSError("something went wrong!")
-
-    g = programl.ProgramGraphToNetworkX(self.unlabelled_graph)
-    return data_flow_graphs.NetworkxDataFlowGraphs([g for _ in range(n or 1)])
-
-
 class TimeoutAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
   """An analysis that times out."""
 
-  def __init__(self, *args, seconds: int = int(1e6), **kwargs):
-    super(TimeoutAnnotator, self).__init__(*args, **kwargs)
+  def __init__(self, seconds: int = int(1e6)):
     self.seconds = seconds
 
   def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
+    self, unlabelled_graph: programl_pb2.ProgramGraph, n: Optional[int] = None
+  ) -> Iterable[programl_pb2.ProgramGraph]:
     time.sleep(self.seconds)
-    return data_flow_graphs.NetworkxDataFlowGraphs([])
-
-
-class ErrorAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis that raises an error."""
-
-  def MakeAnnotated(
-    self, n: Optional[int] = None
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    raise OSError("something went wrong!")
-
-
-class EmptyAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
-  """An analysis which produces no outputs."""
-
-  def MakeAnnotated(
-    self, n: int = 0
-  ) -> data_flow_graphs.NetworkxDataFlowGraphs:
-    return data_flow_graphs.NetworkxDataFlowGraphs([])
+    yield programl_pb2.ProgramGraph()
