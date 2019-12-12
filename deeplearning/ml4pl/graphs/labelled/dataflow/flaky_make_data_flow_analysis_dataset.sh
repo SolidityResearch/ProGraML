512668d56117bdcc6bb81087e84cf7047056c42b
Chris Cummins <chrisc.101@gmail.com>
2019-12-12 00:05:41 +0000

    Add a flaky dataset generator wrapper script.
    [Exported from 6279491c7c340b6e674bade2da4a178adfe5f515]

--- deeplearning/ml4pl/graphs/labelled/dataflow/flaky_make_data_flow_analysis_dataset.sh
+++ deeplearning/ml4pl/graphs/labelled/dataflow/flaky_make_data_flow_analysis_dataset.sh
@@ -17,10 +17,9 @@ main() {
     i=$((i+1))
     echo "Beginning run $i of dataset generator"
     # Run the dataset generator for 30 minutes.
-    if timeout -s9 1800 deeplearning/ml4pl/graphs/labelled/dataflow/make_data_flow_analysis_dataset \
-        --max_instances=10000 \
-        --vmodule='*'=3 \
-        $@ ; then
+    timesout -s9 1800 deeplearning/ml4pl/graphs/labelled/dataflow/make_data_flow_analysis_dataset $@
+    # If the dataset generator completed with a zero return code, terminate.
+    if $@ ; then
       echo "Dataset generator terminated gracefully after $i iterations"
       break
     fi
