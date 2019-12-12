1fc89c70bc3a2263c3ec97d6c72885aa92a28024
Chris Cummins <chrisc.101@gmail.com>
2019-12-12 02:15:07 +0000

    Add comparison benchmark for analyses.
    [Exported from 2359b14416c885d98b72a84866728badf481f623]

--- deeplearning/ml4pl/graphs/labelled/dataflow/annotators_benchmark_test.py
+++ deeplearning/ml4pl/graphs/labelled/dataflow/annotators_benchmark_test.py
@@ -1,23 +1,7 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Benchmarks for comparing annotator performance."""
 from deeplearning.ml4pl.graphs.labelled.dataflow import annotate
 from deeplearning.ml4pl.graphs.labelled.dataflow import data_flow_graphs
 from deeplearning.ml4pl.testing import random_programl_generator
-from labm8.py import prof
 from labm8.py import test
 
 FLAGS = test.FLAGS
@@ -27,24 +11,19 @@ MODULE_UNDER_TEST = None
 PYTEST_ARGS = ["--benchmark-warmup-iterations=2"]
 
 # Real programs to test over.
-PROTOS = list(random_programl_generator.EnumerateTestSet(n=20))
+PROTOS = list(random_programl_generator.EnumerateProtoTestSet(n=20))
 
 # The annotators to test.
 ANNOTATORS = {
-  analysis: annotate.ANALYSES[analysis]
+  analysis: annotate.ANALYSES[analysis]()
   for analysis in annotate.AVAILABLE_ANALYSES
 }
 
 
-def AnnotatorBenchmark(annotator_class):
+def AnnotatorBenchmark(annotator):
   """A micro-benchmark that runs annotator over a list of test graphs."""
-  with prof.Profile(
-    f"Completed benchmark of {len(PROTOS) * 5} annotations "
-    f"using {annotator_class.__name__}"
-  ):
-    for graph in PROTOS:
-      annotator = annotator_class(graph)
-      annotator.MakeAnnotated(5)
+  for graph in PROTOS:
+    annotator.MakeAnnotated(graph, n=5)
 
 
 @test.Parametrize(
