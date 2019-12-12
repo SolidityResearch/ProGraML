b98d353cb0c6bfcdc0b69564cd67576ae342840f
Chris Cummins <chrisc.101@gmail.com>
2019-12-12 17:01:23 +0000

    Add a script to run end-to-end devmap.
    [Exported from 5bd4319e7b6870f7233b766dce35f50c5dbc28e7]

--- deeplearning/ml4pl/experiments/devmap/run_models.py
+++ deeplearning/ml4pl/experiments/devmap/run_models.py
@@ -1,18 +1,3 @@
-# Copyright 2019 the ProGraML authors.
-#
-# Contact Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Run the device mapping models.
 
 Usage:
@@ -21,7 +6,7 @@ Usage:
         --db_stem='sqlite:////tmp/programl/db/'
         --dataset=amd,nvidia \
         --model=zero_r,lstm_opencl,lstm_ir,lstm_inst2vec,ggnn \
-        --tag_suffix=v1
+        --tag_suffix=v1 \
 """
 import time
 
@@ -29,6 +14,7 @@ from deeplearning.ml4pl.graphs.labelled import graph_tuple_database
 from deeplearning.ml4pl.ir import ir_database
 from deeplearning.ml4pl.models import log_database
 from deeplearning.ml4pl.models import run
+from deeplearning.ml4pl.models import schedules
 from deeplearning.ml4pl.models.ggnn import ggnn
 from deeplearning.ml4pl.models.lstm import lstm
 from deeplearning.ml4pl.models.zero_r import zero_r
@@ -54,7 +40,7 @@ app.DEFINE_string(
 )
 app.DEFINE_string(
   "db_stem",
-  "file:///var/phd/db/cc1.mysql?programl",
+  "file:///var/phd/db/cc1.mysql?programl_",
   "The stem for database names.",
 )
 
@@ -64,19 +50,18 @@ def Main():
   db_stem = FLAGS.db_stem
   models = FLAGS.model
   tag_suffix = FLAGS.tag_suffix
-  datasets = FLAGS.dataset
 
   # Set model and dataset-invariant flags.
   FLAGS.log_db = flags_parsers.DatabaseFlag(
-    log_database.Database, f"{db_stem}_devmap_logs", must_exist=True
+    log_database.Database, f"{db_stem}_logs", must_exist=True
   )
   FLAGS.ir_db = flags_parsers.DatabaseFlag(
     ir_database.Database, f"{db_stem}_ir", must_exist=True
   )
   FLAGS.k_fold = True
-  FLAGS.test_on = "improvement_and_last"
+  FLAGS.test_on = schedules.TestOn.IMPROVEMENT_AND_LAST
 
-  for dataset in datasets:
+  for dataset in FLAGS.dataset:
     # Set model-invariant flags.
     FLAGS.graph_db = flags_parsers.DatabaseFlag(
       graph_tuple_database.Database,
@@ -92,33 +77,25 @@ def Main():
         run.Run(zero_r.ZeroR)
       elif model == "lstm_opencl":
         FLAGS.epoch_count = 50
-        FLAGS.ir2seq = flags_parsers.EnumFlag(
-          lstm.Ir2SeqType, lstm.Ir2SeqType.OPENCL
-        )
+        FLAGS.ir2seq = lstm.Ir2SeqType.OPENCL
         FLAGS.padded_sequence_length = 1024
         FLAGS.batch_size = 64
         run.Run(lstm.GraphLstm)
       elif model == "lstm_ir":
         FLAGS.epoch_count = 50
-        FLAGS.ir2seq = flags_parsers.EnumFlag(
-          lstm.Ir2SeqType, lstm.Ir2SeqType.LLVM
-        )
+        FLAGS.ir2seq = lstm.Ir2SeqType.LLVM
         FLAGS.padded_sequence_length = 15000
         FLAGS.batch_size = 64
         run.Run(lstm.GraphLstm)
       elif model == "lstm_inst2vec":
         FLAGS.epoch_count = 50
-        FLAGS.ir2seq = flags_parsers.EnumFlag(
-          lstm.Ir2SeqType, lstm.Ir2SeqType.INST2VEC
-        )
+        FLAGS.ir2seq = lstm.Ir2SeqType.INST2VEC
         FLAGS.padded_sequence_length = 15000
         FLAGS.batch_size = 64
         run.Run(lstm.GraphLstm)
       elif model == "ggnn":
-        # Reduced batch size because OOM errors with larger batches on my
-        # NVIDIA GTX 1080 GPU.
-        FLAGS.graph_batch_size = 32
-        FLAGS.epoch_count = 100
+        FLAGS.graph_batch_size = 64
+        FLAGS.epoch_count = 300
         run.Run(ggnn.Ggnn)
       else:
         raise app.UsageError(f"Unknown model: {model}")
