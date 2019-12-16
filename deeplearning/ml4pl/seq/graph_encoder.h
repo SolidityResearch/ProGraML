b2a009b3ba4775c12f3780d2bbc0ae1b1049ae70
Chris Cummins <chrisc.101@gmail.com>
2019-12-16 06:21:25 +0000

    (Breaking) Rewrite graph2seq in C++.
    This replaces the python implementation of graph
    serialization with a C++ binary that combines
    serialization of program graphs with encoding of
    node strings.
    
    This breaks the LSTM, which will need to be
    refactored to use this new interface.
    
    [Exported from b1f9d1e266458ccddf40b80f297eef3fd8b16765]

--- deeplearning/ml4pl/seq/graph_encoder.h
+++ deeplearning/ml4pl/seq/graph_encoder.h
@@ -1,18 +1,3 @@
-// Copyright 2019 the ProGraML authors.
-//
-// Contact Chris Cummins <chrisc.101@gmail.com>.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 #pragma once
 
 #include <vector>
@@ -25,13 +10,13 @@ namespace ml4pl {
 
 class GraphEncoder {
  public:
-  GraphEncoder(const CachedStringEncoder& string_encoder)
+  GraphEncoder(CachedStringEncoder& string_encoder)
       : string_encoder_(string_encoder) {}
 
   ProgramGraphSeq Encode(const ProgramGraph& graph);
 
  private:
-  const CachedStringEncoder string_encoder_;
+  CachedStringEncoder string_encoder_;
 };
 
 }  // namespace ml4pl
